28b85409e3f3b96aeb75514d74134ff2b9e60b48
Neto Santos <hunaldoneto07@gmail.com>
2024-07-26 12:11:51 -0300

    feat: add biome

--- src/server/routes/index.ts
+++ src/server/routes/index.ts
@@ -3,9 +3,8 @@ import { StatusCodes } from "http-status-codes";
 
 const router = Router();
 
-router.get('/', (req, res) => {
-    return res.status(StatusCodes.CREATED).json({ message: "Hello World!" });
+router.get("/", (req, res) => {
+  return res.status(StatusCodes.CREATED).json({ message: "Hello World!" });
 });
-
 
 export { router };
